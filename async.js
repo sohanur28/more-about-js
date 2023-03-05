4e885b278513e1eb6998a129295e2fd9e6dff2f9
sohanur28 <gsrahman28@gmail.com>
2023-03-05 21:46:11 +0600

    async

---
+++ async.js
@@ -0,0 +1,12 @@
+console.log(1);
+console.log("I");
+setTimeout( () => {
+    console.log("eat");
+}, 2000)
+console.log("Ice Cream");
+console.log(5);
+console.log(6);
+
+function doSomething(){
+    console.log(3);
+}
